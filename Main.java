254343f7a55f1105362cc7daa84c88c3870d896c
Lavanthi <80131633+Lavanthi@users.noreply.github.com>
2025-03-08 12:49:40 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    //created a Game object called gg, which now starts the game
+    Game gg = new Game();
+    gg.playFishBowlGame();
+    }
+}
